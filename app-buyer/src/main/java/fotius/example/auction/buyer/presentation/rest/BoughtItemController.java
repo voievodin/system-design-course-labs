a1e20c36d5172a25afd3eb5f26bfab88ed4543a8
Ihor Pol <igorxlonline24@gmail.com>
2023-04-29 02:50:32 +0300

    lab 3 -- logic (var2)

--- app-buyer/src/main/java/fotius/example/auction/buyer/presentation/rest/BoughtItemController.java
+++ app-buyer/src/main/java/fotius/example/auction/buyer/presentation/rest/BoughtItemController.java
@@ -1,8 +1,6 @@
 package fotius.example.auction.buyer.presentation.rest;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import fotius.example.auction.buyer.domain.BoughtItemService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.MediaType;
@@ -10,28 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 public class BoughtItemController {
 
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class BoughtItemDto {
-        private String description;
-        private BigDecimal price;
-    }
+    private final BoughtItemService boughtItemService;
 
     @GetMapping(path = "/api/{username}/bought-items", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<BoughtItemDto> findAll(@PathVariable("username") String username) {
-        // TODO: change me
-        return List.of(
-            new BoughtItemDto("Description 1", new BigDecimal("100.0")),
-            new BoughtItemDto("Description 2", new BigDecimal("200.0")),
-            new BoughtItemDto("Description 3", new BigDecimal("300.0"))
-        );
+
+        return boughtItemService.findAllByUsername(username).stream()
+                .map(x -> BoughtItemDto.builder()
+                        .description(x.getDescription())
+                        .price(x.getPrice())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
