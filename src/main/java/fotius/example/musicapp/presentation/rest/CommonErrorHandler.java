770271a0bcc363a86067115749323f8ddd330846
BulatNick <bulatnikov.mykyta@vu.cdu.edu.ua>
2023-04-29 09:57:32 +0300

    Second lab done

--- src/main/java/fotius/example/musicapp/presentation/rest/CommonErrorHandler.java
+++ src/main/java/fotius/example/musicapp/presentation/rest/CommonErrorHandler.java
@@ -1,10 +1,12 @@
 package fotius.example.musicapp.presentation.rest;
 
 import fotius.example.musicapp.domain.error.InvalidRequestException;
+import fotius.example.musicapp.domain.error.SongIsAlreadyInPlaylistException;
 import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -43,6 +45,18 @@ public class CommonErrorHandler {
                     List.of()
                 )
             );
+    }
+
+    @ExceptionHandler(SongIsAlreadyInPlaylistException.class)
+    public ResponseEntity<ApiError> handleSongIsAlreadyInPlaylistException(SongIsAlreadyInPlaylistException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(
+                        new ApiError(
+                                ex.getMessage(),
+                                LocalDateTime.now(),
+                                List.of()
+                        )
+                );
     }
 
     @ExceptionHandler(Exception.class)
