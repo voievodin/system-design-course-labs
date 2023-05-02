0577304080a52ec9947a5dd9928e3eddac066ab2
Savienkov Maksym <froggt25@gmail.com>
2023-05-02 20:47:06 +0300

    Little changes

--- src/main/java/fotius/example/donations/webhook/domain/AccessWebhooksOnPaymentStateChange.java
+++ src/main/java/fotius/example/donations/webhook/domain/AccessWebhooksOnPaymentStateChange.java
@@ -2,15 +2,8 @@ package fotius.example.donations.webhook.domain;
 
 import fotius.example.donations.payment.domain.PaymentChangeListener;
 import fotius.example.donations.payment.domain.model.Payment;
-import fotius.example.donations.webhook.domain.model.Webhook;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
 
 @Component
 @AllArgsConstructor
@@ -22,46 +15,5 @@ public class AccessWebhooksOnPaymentStateChange implements PaymentChangeListener
     public void onChanged(Payment changed) {
         webhookService.getAllByMethodAndState(changed.getMethod(), changed.getState())
                 .forEach(webhook -> System.out.println("Send message on " + webhook.getTargetUrl()));
-    }
-
-    private static void sendPOST(Webhook webhook, String message) throws IOException {
-        ////Or:
-        //try {
-        //    sendPOST(webhook, "Payment " + changed.getId() + " changed. " +
-        //            "Payment method: " + changed.getMethod() + "; " +
-        //            "Payment state: " + changed.getState() + ";");
-        //} catch (IOException e) {
-        //    throw new RuntimeException(e);
-        //}
-
-        HttpURLConnection con = (HttpURLConnection) webhook.getTargetUrl().openConnection();
-        con.setRequestMethod("POST");
-
-        // For POST only - START
-        con.setDoOutput(true);
-        OutputStream os = con.getOutputStream();
-        os.write(("message=" + message).getBytes());
-        os.flush();
-        os.close();
-        // For POST only - END
-
-        int responseCode = con.getResponseCode();
-        System.out.println("POST Response Code :: " + responseCode);
-
-        if (responseCode == HttpURLConnection.HTTP_OK) { //success
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // print result
-            System.out.println(response);
-        } else {
-            System.out.println("POST request did not work.");
-        }
     }
 }
