28281d786da0384ad9f6758a992ba8e93b5f2d2e
Vlad-Polygalov <vladpoligalov3@gmail.com>
2023-05-07 12:48:52 +0300

    complete lab2

---
+++ src/main/java/fotius/example/musicapp/presentation/rest/SongController.java
@@ -0,0 +1,130 @@
+package fotius.example.musicapp.presentation.rest;
+
+import fotius.example.musicapp.domain.SongService;
+import fotius.example.musicapp.domain.model.Song;
+import lombok.*;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/songs")
+@RequiredArgsConstructor
+public class SongController {
+
+    private final SongService songService;
+
+    @PostMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public Song createSong(@RequestParam("name") String name, @RequestParam("artist") String artist, @RequestParam("location") String location, @RequestParam("lyrics") String lyrics) {
+        return songService
+                .create(SongService
+                        .CreateSongCommand
+                        .builder()
+                        .artist(artist)
+                        .location(location)
+                        .lyrics(lyrics)
+                        .name(name)
+                        .build());
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public Song getSong(@PathVariable("id") Long id) {
+        return songService.getById(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PATCH,
+            path = "{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public Song updateSong(@PathVariable("id") Long id, @RequestBody() SongUpdateRequest request) {
+        return songService
+                .update(id, SongService.
+                        UpdateSongCommand
+                        .builder()
+                        .location(request.location)
+                        .addedLikes(Integer.parseInt(request.likes))
+                        .lyrics(request.lyrics)
+                        .build());
+    }
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            path = "{id}"
+    )
+    @ResponseBody
+    public void deleteSong(@PathVariable("id") Long id) {
+        songService.delete(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET
+    )
+    @ResponseBody
+    public SongsPage getAllWithArtist(
+            @RequestParam(name = "artist", defaultValue = "") String artist,
+            @RequestParam(name = "sort", defaultValue = "ASC") Sort.Direction sort,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "perPage", defaultValue = "10") int perPage
+    ) {
+        Slice<Song> serviceResponse = !Objects.equals(artist, "")
+                ? songService.findByArtistOrderedByAddedAt(artist, page, perPage, sort) :
+                songService.findAllOrderedByAddedAt(page, perPage, sort);
+
+        UriComponents currentUrl = UriComponentsBuilder.fromPath("/api/songs")
+                .queryParam("artist", artist)
+                .queryParam("sort", sort)
+                .queryParam("page", "{page}")
+                .build();
+
+        return SongsPage
+                .builder()
+                .songs(serviceResponse.getContent())
+                .pageNumber(page)
+                .nextPageUrl(serviceResponse.hasNext()
+                        ? currentUrl.expand(page + 1).toUriString()
+                        : null)
+                .previousPageUrl(serviceResponse.hasPrevious()
+                        ? currentUrl.expand(page - 1).toUriString()
+                        : null)
+                .build();
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SongsPage {
+        private List<Song> songs;
+        private int pageNumber;
+        private String nextPageUrl;
+        private String previousPageUrl;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SongUpdateRequest {
+        private String location;
+        private String likes;
+        private String lyrics;
+    }
+
+}
